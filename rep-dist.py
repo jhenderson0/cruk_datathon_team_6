80dc38335bf17a0e24e46f6a78c8fb5e29b43243
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-01-23 23:53:20 +0000

    Get epitope distribution

--- rep-dist.py
+++ rep-dist.py
@@ -2,13 +2,19 @@ import plotly.graph_objects as go
 import numpy as np
 import torch
 import torch.nn.functional as F
+import numpy.typing as npt
+
+def get_dist(conditional: npt.NDArray) -> npt.NDArray:
+    one_hot = (conditional == conditional.max(axis=1)[:,None]).astype(int)
+    counts = np.sum(one_hot, axis=0)
+    norm = np.sum(counts)
+    return counts / np.full(counts.shape, norm)
+
 
 if __name__ == "__main__":
-    data = torch.load("./ignore/all_probability.pt", weights_only=True)
-    data = data.numpy()
-    one_hot = (data == data.max(axis=1)[:,None]).astype(int)
-    counts = np.sum(one_hot, axis=0)
-    print(counts)
+    normal = torch.load("./ignore/all_probability.pt", weights_only=True).numpy()
+    norm_dist = get_dist(normal)
+    print(norm_dist)
 
     # fig = go.Figure()
     #
