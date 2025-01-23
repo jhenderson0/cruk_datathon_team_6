23fe5faa8c0735c8c94f2373a7d2e1780d91a7cd
MVCowley <51127523+MVCowley@users.noreply.github.com>
2025-01-23 22:28:39 +0000

    Run dumpy on tcrs from decombinator output

--- predict.py
+++ predict.py
@@ -24,17 +24,23 @@ def compute_epitope_priors(epi_length: int, num_ones_in_alphabet: int) -> dict:
     return {epitope: compute_prob(epitope) for epitope in possible_epitopes}
 
 
-def get_crd3(path: str, alphabet: pd.DataFrame) -> npt.NDArray:
+def get_crd3(path: str, alphabet: pd.DataFrame) -> torch.Tensor:
     """
     Get cdr3 from Decombinator output, strip, and translate to binary.
     """
     df = pd.read_csv(path, compression="gzip", header=0, sep="\t")
     cdr3s = df["junction_aa"].dropna().unique().tolist()
-    fifteenmers = [cdr3 for cdr3 in cdr3s if len(cdr3) == 15]
-    shortmers = [mer[4:14] for mer in fifteenmers]
-    translate_dict = {aa: binary["Letter"] for aa, binary in alphabet.to_dict().items()}
-    print(translate_dict)
-    # translation_table = str.maketrans(alphabet)
+    cdr3s = [cdr3 for cdr3 in cdr3s if len(cdr3) == 15]
+    cdr3s = [mer[4:14] for mer in cdr3s]
+    translate_dict = {
+        aa: str(binary["Letter"]) for aa, binary in alphabet.to_dict().items()
+    }
+    translation_table = str.maketrans(translate_dict)
+    cdr3s = [mer.translate(translation_table) for mer in cdr3s]
+    cdr3s = [list(string) for string in cdr3s]
+    cdr3s = [[float(i) for i in mer] for mer in cdr3s]
+    cdr3s = torch.stack([torch.tensor(mer, dtype=torch.float32) for mer in cdr3s])
+    return cdr3s
 
 
 if __name__ == "__main__":
@@ -48,13 +54,8 @@ if __name__ == "__main__":
     joint_model = DumpyJoint(model, epitope_priors)
 
     cdr3s = get_crd3("./ignore/dcr_LTX_0001_N_beta.tsv.gz", alphabet)
+    print(cdr3s)
 
-    all_possible_tcrs = torch.stack(
-        [
-            torch.tensor(prod, dtype=torch.float32)
-            for prod in itertools.product([0, 1], repeat=10)
-        ]
-    )
     all_possible_epitopes = torch.stack(
         [
             torch.tensor(epitope, dtype=torch.float32)
@@ -62,9 +63,10 @@ if __name__ == "__main__":
         ]
     )
 
-    mini_tcr = all_possible_tcrs[:5]
+    mini_tcr = cdr3s[:5]
     mini_epitope = all_possible_epitopes[:5]
 
     predict = joint_model.joint(mini_tcr, mini_epitope)
+    print(predict)
 
     # Get vs all epitopes and normalise one of interest by whole row
